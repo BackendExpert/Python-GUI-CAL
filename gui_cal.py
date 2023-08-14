b38699175db720d213794159ab40d108cbfcadf9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-14 19:24:15 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- gui_cal.py
+++ gui_cal.py
@@ -18,13 +18,16 @@ answer_text = ""
 
 cal_font = ('Arial', 20, 'bold')
 
-answer_feild = customtkinter.CTkEntry(master=app, show=answer_text, width=500, height=50, font=cal_font)
-answer_feild.grid(sticky=)
+answer_feild = customtkinter.CTkEntry(master=app, show=answer_text, width=480, height=50, font=cal_font)
+answer_feild.grid(padx=10, pady=10)
 
 button_clear = customtkinter.CTkButton(master=app, text="C", text_color="#ffffff",width=60, height=60, font=cal_font, fg_color="#b5520b",hover_color="#2e2a27")
 button_clear.place(x=10,y=100)
 
-button_
+button_add = customtkinter.CTkButton(master=app, command=lambda:show("+"), text="+", text_color="#ffffff",width=60, height=60, font=cal_font, fg_color="#b5520b",hover_color="#2e2a27")
+button_add.place(x=10,y=100)
+
+
 
 
 # testButton = customtkinter.CTkButton(master=app, text="JehanKandy")
