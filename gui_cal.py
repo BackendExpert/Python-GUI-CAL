b322ae4f866f59f2f0e43e8da472fc2574dfa338
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-14 18:12:35 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- gui_cal.py
+++ gui_cal.py
@@ -3,6 +3,9 @@ import customtkinter
 
 app = customtkinter.CTk()
 
+app.geometry("500x600")
 
+testButton = customtkinter.CTkButton(master=app, text="JehanKandy")
+testButton.place(relx=0.5, rely=0.5, anchor="center")
 
 app.mainloop()
