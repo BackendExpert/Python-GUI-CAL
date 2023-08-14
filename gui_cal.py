3e9ebc6a735ca280627b071147556953e143520d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-14 19:07:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- gui_cal.py
+++ gui_cal.py
@@ -13,15 +13,14 @@ app = customtkinter.CTk()
 app.geometry("500x600")
 app.title("GUI Scientific Calculator")
 
-main_frame = customtkinter.CTkFrame(master=app, width=450, height=550)
-main_frame.pack(expand= True, padx= 10, pady=20)
 
 answer_text = ""
 
 cal_font = ('Arial', 20, 'bold')
 
-answer_feild = customtkinter.CTkEntry(master=main_frame, show=answer_text)
-answer_feild.grid_columnconfigure(0, weight=1)
+answer_feild = customtkinter.CTkEntry(master=app, show=answer_text, width=500, height=50, font=cal_font)
+answer_feild.place(x=0, y=10)
+
 
 # testButton = customtkinter.CTkButton(master=app, text="JehanKandy")
 # testButton.place(relx=0.5, rely=0.5, anchor="center")
