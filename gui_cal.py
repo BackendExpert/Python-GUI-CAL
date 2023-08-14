197f85b01c1ebaa8587b9805fabffcc4eb8c732c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-14 18:00:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- gui_cal.py
+++ gui_cal.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 app = tk.Tk()
 
-testButton = tk.Button(app, text="Hello World")
+testButton = tk.Button(app, text="Hello World", command=app.destroy)
 testButton.pack()
 
 app.mainloop()
