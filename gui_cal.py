85323786e9a2839c11f898d99591b554ac12e6a6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2023-08-14 19:13:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- gui_cal.py
+++ gui_cal.py
@@ -21,6 +21,9 @@ cal_font = ('Arial', 20, 'bold')
 answer_feild = customtkinter.CTkEntry(master=app, show=answer_text, width=500, height=50, font=cal_font)
 answer_feild.place(x=0, y=10)
 
+button1 = customtkinter.CTkButton(master=app,command=lambda:show("1"), text="1", text_color="#ffffff",width=50, height=2, fg_color="#b5520b",hover_color="#2e2a27")
+button1.place(x=10,y=100)
+
 
 # testButton = customtkinter.CTkButton(master=app, text="JehanKandy")
 # testButton.place(relx=0.5, rely=0.5, anchor="center")
